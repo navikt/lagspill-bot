04b2ff8d440191dc1168d81cc727db6c3aca1a7f
Tor Idland <tor.idland@nav.no>
2024-08-17 19:26:04 +0200

    prøver node-postgres driver

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,14 +1,30 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { logger } from '@navikt/next-logger'
 import { lazyNextleton } from 'nextleton'
 
 export const prisma = lazyNextleton('prisma', () => {
+
+    const connectionString = `${process.env.NAIS_DATABASE_LAGSPILL_BOT_LAGSPILL_BOT_URL}`
+
+    const pool = new Pool({ connectionString })
+    const adapter = new PrismaPg(pool)
     const client = new PrismaClient({
+        adapter,
         log: [
             { emit: 'event', level: 'warn' },
             { emit: 'event', level: 'error' },
         ],
     })
+
+
+    // const client = new PrismaClient({
+    //     log: [
+    //         { emit: 'event', level: 'warn' },
+    //         { emit: 'event', level: 'error' },
+    //     ],
+    // })
 
     client.$on('error', (e) => {
         logger.error(e)
