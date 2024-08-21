dcc4a1b120fa7e78cd32d220d1f2f61089dbef9c
Tor Idland <tor.idland@nav.no>
2024-08-21 16:37:13 +0200

    fjerner node-postgres

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,18 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { logger } from '@navikt/next-logger'
 import { lazyNextleton } from 'nextleton'
-import {isLocal} from "../utils/env";
 
 export const prisma = lazyNextleton('prisma', () => {
-
-    const connectionString = `${process.env.LAGSPILL_BOT_DB_URL}`
-
-    const pool = new Pool({ connectionString })
-    const adapter = new PrismaPg(pool)
     const client = new PrismaClient({
-        adapter,
         log: [
             { emit: 'event', level: 'warn' },
             { emit: 'event', level: 'error' },
