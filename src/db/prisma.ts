dcdc118c1a6b34008b675311a442597bbfd96774
Tor Idland <tor.idland@nav.no>
2024-08-21 13:58:51 +0200

    echo db url

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -7,9 +7,7 @@ import {isLocal} from "../utils/env";
 
 export const prisma = lazyNextleton('prisma', () => {
 
-    const connectionString = isLocal
-        ? `${process.env.LAGSPILL_BOT_DB_URL}`
-        : `postgresql://${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_USERNAME}:${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_PASSWORD}@${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_HOST}:${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_PORT}/${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_DATABASE}?sslcert=..${process.env.NAIS_LAGSPILL_BOT_LAGSPILL_BOT_SSLCERT}`
+    const connectionString = `${process.env.LAGSPILL_BOT_DB_URL}`
 
     const pool = new Pool({ connectionString })
     const adapter = new PrismaPg(pool)
